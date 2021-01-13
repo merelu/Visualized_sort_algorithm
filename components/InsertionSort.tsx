c604f6d35f78d05af6d639de86bbd03d9ace0668
GYUHA_HWANG <gyuha0310@gmail.com>
2021-01-13 23:09:51 +0900

    sort 기능 추가

--- components/InsertionSort.tsx
+++ components/InsertionSort.tsx
@@ -5,6 +5,23 @@ import { useState } from "react";
 const getArr = () => {
   return shuffle(range(1, 11));
 };
+const swap = (arr: number[], a: number, b: number) => {
+  const tmp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp;
+};
+const sort = (arr: number[]) => {
+  //https://en.wikipedia.org/wiki/Insertion_sort
+  let i = 1;
+  while (i < arr.length) {
+    let j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      swap(arr, j, j - 1);
+      j = j - 1;
+    }
+    i = i + 1;
+  }
+};
 
 export default () => {
   const [arr, setArr] = useState(getArr());
@@ -12,13 +29,18 @@ export default () => {
   const handleShuffle = () => {
     setArr(getArr());
   };
+  const handleSort = () => {
+    const sortedArr = [...arr];
+    sort(sortedArr);
+    setArr(sortedArr);
+  };
   return (
     <>
       <div className={styles.board}>{arr.join(",")}</div>
 
       <div className={styles.buttonBox}>
         <button onClick={handleShuffle}>shuffle</button>
-        <button>sort</button>
+        <button onClick={handleSort}>sort</button>
       </div>
     </>
   );
