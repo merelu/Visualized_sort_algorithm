5d9e2228d6fe20b795cf395dffd602e6db4a0d6b
GYUHA_HWANG <gyuha0310@gmail.com>
2021-01-13 22:38:00 +0900

    implement layout

--- components/InsertionSort.tsx
+++ components/InsertionSort.tsx
@@ -1,1 +1,16 @@
-export default () => <div>Insertion Sort</div>;
+import styles from "../styles/Home.module.css";
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export default () => {
+  return (
+    <>
+      <div className={styles.board}>{arr.join(",")}</div>
+
+      <div className={styles.buttonBox}>
+        <button>shuffle</button>
+        <button>sort</button>
+      </div>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Visualized Sort Algorithm</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Visualized Sort Algorithm</h1>
         <InsertionSort></InsertionSort>
       </main>
     </div>
